5279f1785d03dea45a6c80a055c4f2149133d1f9
sudip101-arc <maitysudip6524@gmail.com>
2022-03-14 07:48:04 +0530

    Added loader

--- src/App.js
+++ src/App.js
@@ -1,68 +1,82 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Recipe from './recipe';
-import Header from './header';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Recipe from "./recipe";
+import Header from "./header";
+import axios from "axios";
+import Loader from "./Loader";
 
 const App = () => {
-  
   const APP_ID = "e67af003";
   const APP_KEY = "cec10aed796957cfa07abda75963a6ce";
 
   // const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-  
+
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState('chicken');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
-    getRecipes();
+    axios
+      .get(
+        `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      )
+      .then((response) => {
+        console.log(response);
+        setLoading(false);
+        setRecipes(response.data.hits);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(true);
+      });
   }, [query]);
 
-  const getRecipes = async () => {
-    const response = await fetch (
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-    );
-    const data = await response.json();
-    setRecipes(data.hits);
-    console.log(data.hits);
-  };
-
-  const updateSearch = e => {
+  const updateSearch = (e) => {
     setSearch(e.target.value);
-  }
-
-  const getSearch = e => {
-    e.preventDefault();
-    setQuery(search);
-    setSearch('');
   };
 
-  return(
+  const getSearch = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setQuery(search);
+    setSearch("");
+  };
+
+  return (
     <div className="App">
       <Header />
       <form onSubmit={getSearch} className="search-form">
         <h2>Your favourite recipe here</h2>
         <div className="search-box">
-          <input type="text" className="search-bar" value={search} onChange={updateSearch}/>
+          <input
+            type="text"
+            className="search-bar"
+            value={search}
+            onChange={updateSearch}
+          />
           <button className="search-btn">search</button>
         </div>
       </form>
       <div className="recipe-container">
-        {recipes.map(recipe => (
-          <Recipe
-            key={recipe.recipe.label}
-            title={recipe.recipe.label}
-            calories={recipe.recipe.calories}
-            image={recipe.recipe.image}
-            ingredients={recipe.recipe.ingredients}
-          />
-        ))}
+        {loading ? (
+          <Loader />
+        ) : (
+          recipes.map((recipe) => (
+            <Recipe
+              key={recipe.recipe.label}
+              title={recipe.recipe.label}
+              calories={recipe.recipe.calories}
+              image={recipe.recipe.image}
+              ingredients={recipe.recipe.ingredients}
+            />
+          ))
+        )}
+        {error ? <p>Can't load. Try again...</p> : null}
       </div>
     </div>
   );
 };
-
 
 export default App;
--- src/recipe.js
+++ src/recipe.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import './recipe.css';
+import React from "react";
+import "./recipe.css";
 
-const Recipe = ({title, calories, image, ingredients}) => {
-
-    return(
-        <div className="recipe-box">
-            <h1>{title}</h1>
-            <ol>
-                {ingredients.map(ingredient => (
-                    <li>{ingredient.text}</li>
-                ))}
-            </ol>
-            <div className="content">
-                <p><span>Calories - </span>{calories}</p>
-                <img src={image} alt="" />
-            </div>
-        </div>
-    );
+const Recipe = ({ title, calories, image, ingredients }) => {
+  return (
+    <div className="recipe-box">
+      <h1>{title}</h1>
+      <ol>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ol>
+      <div className="content">
+        <p>
+          <span>Calories - </span>
+          {calories}
+        </p>
+        <img src={image} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Recipe;
